834c2cac5c094005e7f8c0facd603962b7f79c54
vizels <mirchuk.georgiy@gmail.com>
2024-04-20 16:36:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Frozen;
+using System.Net;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace API_BD
+{
+    internal class Program
+    {
+        static async Task<Song> GetSong(string author, string title)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    string URL = $"https://api.lyrics.ovh/v1/{author}/{title}";
+                    string response = await httpClient.GetStringAsync(URL);
+                    //string responseBody = await response.Content.ReadAsStringAsync();
+                    Song song = JsonSerializer.Deserialize<Song>(response);
+                    song.author = author;
+                    song.title = title;
+                    song.lyrics = song.lyrics.Substring(song.lyrics.IndexOf('\n') + 1, song.lyrics.Length - song.lyrics.IndexOf('\n')-1);
+
+                    //Console.WriteLine("Response:");
+                    //Console.WriteLine(responseBody);
+                    //Console.WriteLine(song.ToString());
+                    return song;
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                
+
+            }
+
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Song test1 = GetSong("Metallica", "Here comes revenge").Wait();
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_BD
+{
+    internal class Song
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string lyrics { get; set; }
+
+        public override string ToString()
+        {
+            return $"{author} - {title}\nLyrics:\n{lyrics}";
+        }
+    }
+}
